2cdc98cb2e026dc392001af871464e6f138a91eb
nougght <muhamedmakusev@gmail.com>
2026-09-08 21:05:17 +0300

    fix(agent-ui): update agent port

--- agent-ui/orval.config.ts
+++ agent-ui/orval.config.ts
@@ -1,12 +1,12 @@
 export default {
     agent: {
-        input: 'http://127.0.0.1:8088/swagger/doc.json',
+        input: 'http://127.0.0.1:8111/swagger/doc.json',
         output: {
             mode: 'split',
             schemas: 'src/api/models',
             target: 'src/api/client',
             client: 'fetch',
-            baseUrl: 'http://127.0.0.1:8088',
+            baseUrl: 'http://127.0.0.1:8111',
         },
     },
 };
